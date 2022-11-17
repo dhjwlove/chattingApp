08e01a447e129a86d402240ae804af9eb8212496
dhjwlove <dhjwlove@gmail.com>
2022-11-17 21:14:57 +0900

    fix webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import * as ReactDOM from "react-dom/client";
 
 const domContainer = document.querySelector("#root");
 
 let root = ReactDOM.createRoot(domContainer);
 
-root.render(<div>test</div>);
+root.render(<div>testtest</div>);
--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -29,6 +29,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
+      template: "./public/index.html"
     }),
     new webpack.HotModuleReplacementPlugin(),
   ],
