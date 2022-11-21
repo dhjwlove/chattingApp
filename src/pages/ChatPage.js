d921f0e23371dc4702f7a1317f1e292650cbe9df
dhjwlove <dhjwlove@gmail.com>
2022-11-21 09:06:20 +0900

    add css Setting

---
+++ src/pages/ChatPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import css from './ChatPage.css'
+
+export default function ChatPage () {
+    return (
+    <div>
+        <h1 className='title'>ChatPage</h1>
+        
+    </div>)    
+}
